85fb63cadd72ebed915226878a074fde28bf77d3
Ivan <taktaef@yandex.ru>
2025-03-06 14:04:27 +0300

    add finish notification

--- dlp.sh
+++ dlp.sh
@@ -4,3 +4,5 @@ echo "Add url to file:"
 read user_input
 
 yt-dlp --cookies-from-browser firefox "$user_input" -P ~/Downloads -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
+echo "Done! You can close terminal now"
+exit
