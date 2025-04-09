a2cf8461c1c319c39259c1358bb0f65a5192934d
Ivan Taktaev <taktaef@yandex.ru>
2025-04-09 13:11:03 +0300

    add audio download

---
+++ dlp-audio.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Add url to file:"
+read user_input
+
+yt-dlp --cookies-from-browser firefox "$user_input" -P ~/Downloads -x --audio-format best --audio-quality 0
+echo "Done! You can close terminal now"
+exit
---
+++ yt-dlp-quick-audio.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+konsole --noclose -e /bin/bash -c 'bash <(curl -s https://raw.githubusercontent.com/Greaterist/yt-dlp-quick/refs/heads/main/dlp-audio.sh)';
