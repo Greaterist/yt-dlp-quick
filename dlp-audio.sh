b43cd5145ca51b1beef2b14863acddea9af8b93c
Ivan Taktaev <taktaef@yandex.ru>
2025-04-09 13:22:25 +0300

    remove symbols at the end audio

--- dlp-audio.sh
+++ dlp-audio.sh
@@ -3,6 +3,6 @@
 echo "Add url to file:"
 read user_input
 
-yt-dlp --cookies-from-browser firefox "$user_input" -P ~/Downloads -x --audio-format mp3 --audio-quality 0 --embed-thumbnail --convert-thumbnails jpg
+yt-dlp --cookies-from-browser firefox "$user_input" -P ~/Downloads -x --audio-format mp3 --audio-quality 0 --embed-thumbnail --convert-thumbnails jpg --no-simulate
 echo "Done! You can close terminal now"
 exit
