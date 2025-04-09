c75b0b9706ccd97da4067eb30c1775601e79a4c7
Ivan Taktaev <taktaef@yandex.ru>
2025-04-09 13:27:25 +0300

    test audio filename

--- dlp-audio.sh
+++ dlp-audio.sh
@@ -3,6 +3,6 @@
 echo "Add url to file:"
 read user_input
 
-yt-dlp --cookies-from-browser firefox "$user_input" -P ~/Downloads -x --audio-format mp3 --audio-quality 0 --embed-thumbnail --convert-thumbnails jpg -o "%(title)s.%(ext)s"
+yt-dlp --cookies-from-browser firefox "$user_input" -P ~/Downloads -o "%(title)s.%(ext)s" -x --audio-format mp3 --audio-quality 0 --embed-thumbnail --convert-thumbnails jpg
 echo "Done! You can close terminal now"
 exit
