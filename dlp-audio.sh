bfdbd17b9cd2cd676dbd7d8fb78aced7dc823bc2
Ivan Taktaev <taktaef@yandex.ru>
2025-04-09 13:16:47 +0300

    add audio cover

--- dlp-audio.sh
+++ dlp-audio.sh
@@ -3,6 +3,6 @@
 echo "Add url to file:"
 read user_input
 
-yt-dlp --cookies-from-browser firefox "$user_input" -P ~/Downloads -x --audio-format mp3 --audio-quality 0
+yt-dlp --cookies-from-browser firefox "$user_input" -P ~/Downloads -x --audio-format mp3 --audio-quality 0 --embed-thumbnail --convert-thumbnails jpg
 echo "Done! You can close terminal now"
 exit
